53a186f966de9b11bcf9377df1735a5b39edc91d
qpm <kangqiang.w@gmail.com>
2020-09-18 14:44:34 +0800

    feat:增加一个颜色处理的包

--- ssh_test/ssh_test.py
+++ ssh_test/ssh_test.py
@@ -21,11 +21,12 @@ class MySSHClient():
 
     def __init__(self):
         self.ssh_client = SSHClient()
-    
+
     def ssh_login(self, host_ip, username, password):
         try:
             self.ssh_client.set_missing_host_key_policy(AutoAddPolicy())
-            self.ssh_client.connect(host_ip, port=22, username=username, password=password)
+            self.ssh_client.connect(
+                host_ip, port=22, username=username, password=password)
         except AuthenticationException:
             log.warning('username or password error')
             return 1001
@@ -41,22 +42,23 @@ class MySSHClient():
     def ssh_logout(self):
         log.warning('will exit host')
         self.ssh_client.close()
-    
+
     def execute_some_command(self, command):
         _, stdout, _ = self.ssh_client.exec_command(command)
         print(stdout.read().decode())
-    
+
 
 if __name__ == "__main__":
     host_ip = "local.qpm.com"
     username = "root"
-    password = ""
+    password = "qpm123"
 
     command = 'tree compose_software'
 
     my_ssh_client = MySSHClient()
     if my_ssh_client.ssh_login(host_ip, username, password) == 1000:
-        log.warning(f"{host_ip} - login success, will execute command: {command}")
+        log.warning(
+            f"{host_ip} - login success, will execute command: {command}")
         my_ssh_client.execute_some_command(command)
         my_ssh_client.ssh_logout()
 
